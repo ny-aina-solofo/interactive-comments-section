abbb16a695a78ce7f69ce931191d83e9e001a0a3
ny-aina-solofo <nyainarandrianarisolo@gmail.com>
2026-02-05 18:24:45 +0300

    chore(server) : initial fastApi configuration

---
+++ server/app/config/database.py
@@ -0,0 +1,29 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_URL = (
+    f"postgresql://{os.getenv('USER_CONNECTION')}:"
+    f"{os.getenv('PASSWORD_CONNECTION')}@"
+    f"{os.getenv('HOST_CONNECTION')}:"
+    f"{os.getenv('PORT_CONNECTION')}/"
+    f"{os.getenv('DATABASE_CONNECTION')}"
+)
+
+engine = create_engine( DATABASE_URL )
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
--- server/server.py
+++ server/server.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from app.api.v1.endpoint import comment
 
 app = FastAPI()
-
+app.include_router(comment.router)
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "app runing"}
